3cfc10b5c4fb6c593a17acb93f06a61c44da7fd4
Kseniia <103669056+MissKseniia@users.noreply.github.com>
2023-09-19 16:43:49 +0400

    Added field injection (death in Needle7); Deleted @Component in Duck5 Corrected the structure in Rabbit4

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,6 +1,9 @@
 package koschei.config;
 
-import koschei.models.*;
+import koschei.models.Duck5;
+import koschei.models.Egg6;
+import koschei.models.Island2;
+import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +18,7 @@ public class AppConfig {
     }
 
     @Bean
-    public static Duck5 getDuck(Egg6 egg6) { return new Duck5(egg6); }
-
-    @Bean
-    public static Needle7 getNeedle(Deth8 deth8) { return new Needle7(deth8); }
+    public static Duck5 getDuck(Egg6 egg6) {
+        return new Duck5(egg6);
+    }
 }
--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class Rabbit4 {
 
+    private Duck5 duck5;
+
     @Autowired
     public void setDuck5(Duck5 duck5) {
         this.duck5 = duck5;
     }
-
-    private Duck5 duck5;
 
     @Override
     public String toString() {
